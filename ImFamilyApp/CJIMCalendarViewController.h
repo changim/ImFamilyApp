937715a19a8e39588ea720ce97e72419986bc31e
Chang Hyun Im <cim@sas.upenn.edu>
2012-12-17 12:10:16 -0500

    ready for demo

---
+++ ImFamilyApp/CJIMCalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CJIMCalendarViewController.h
+//  ImFamilyApp
+//
+//  Created by James Im on 12/17/12.
+//  Copyright (c) 2012 James Im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CJIMCalendarViewController : UIViewController
+
+@end
---
+++ ImFamilyApp/CJIMCalendarViewController.m
@@ -0,0 +1,38 @@
+//
+//  CJIMCalendarViewController.m
+//  ImFamilyApp
+//
+//  Created by James Im on 12/17/12.
+//  Copyright (c) 2012 James Im. All rights reserved.
+//
+
+#import "CJIMCalendarViewController.h"
+
+@interface CJIMCalendarViewController ()
+
+@end
+
+@implementation CJIMCalendarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
