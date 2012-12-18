2be001af9c8649c72bf1318f9c945a188a35fa09
Chang Hyun Im <cim@sas.upenn.edu>
2012-12-18 00:20:02 -0500

    user location implemented

--- ImFamilyApp/CJIMMapViewController.m
+++ ImFamilyApp/CJIMMapViewController.m
@@ -62,12 +62,41 @@
         MKCoordinateRegion region;
         region.center.latitude = [currentUserLocation.latitude doubleValue];
         region.center.longitude = [currentUserLocation.longitude doubleValue];
-        region.span.latitudeDelta = .2;
-        region.span.longitudeDelta = .2;
+        region.span.latitudeDelta = .02;
+        region.span.longitudeDelta = .02;
         region = [self.mapView regionThatFits:region];
         [self.mapView setRegion:region animated:TRUE];
     }
 }
+
+- (IBAction)centerMapToUserLocation:(UIButton *)sender {
+    CJIMAppDelegate *appDelegate = ((CJIMAppDelegate *)[[UIApplication sharedApplication] delegate]);
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"User" inManagedObjectContext:appDelegate.managedObjectContext];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name like %@", sender.titleLabel.text];
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entity];
+    [request setPredicate:predicate];
+    
+    NSError *error = nil;
+    NSArray *queryResult = [appDelegate.managedObjectContext executeFetchRequest:request error:&error];
+    if (queryResult.count!=1) {
+        NSLog(@"did not find one user with such name");
+        return;
+    } else {
+        User *user = (User*) [queryResult objectAtIndex:0];
+        
+        [self.mapView setCenterCoordinate: CLLocationCoordinate2DMake([user.location.latitude doubleValue],[user.location.longitude doubleValue]) animated:YES];
+        
+        MKCoordinateRegion region;
+        region.center.latitude = [user.location.latitude doubleValue];
+        region.center.longitude = [user.location.longitude doubleValue];
+        region.span.latitudeDelta = .02;
+        region.span.longitudeDelta = .02;
+        region = [self.mapView regionThatFits:region];
+        [self.mapView setRegion:region animated:TRUE];
+    }
+}
+
 
 
 - (void) prepareMapPresentation {
